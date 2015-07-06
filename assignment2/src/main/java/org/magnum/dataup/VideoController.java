8484b4ca708c4b38818260a737169e27ec40fe62
tdongsi <dongsi.tuecuong@gmail.com>
2015-07-05 21:41:38 -0700

    Coursera: Spring: Assignment 2: Implement POST /video/{id}/data

--- assignment2/src/main/java/org/magnum/dataup/VideoController.java
+++ assignment2/src/main/java/org/magnum/dataup/VideoController.java
@@ -17,6 +17,7 @@
  */
 package org.magnum.dataup;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,18 +26,26 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.servlet.http.HttpServletRequest;
 
 import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class VideoController {
 
 	private static final AtomicLong currentId = new AtomicLong(0L);
+	private static VideoFileManager videoDataMgr;
 
 	private Map<Long, Video> videos = new HashMap<Long, Video>();
 
@@ -76,6 +85,34 @@ public class VideoController {
 	@RequestMapping(value = { VideoSvcApi.VIDEO_SVC_PATH }, method = { RequestMethod.GET } )
 	public @ResponseBody Collection<Video> getVideoList() {
 		return videos.values();
+	}
+	
+	@ExceptionHandler(IOException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public void handleResourceNotFoundException(IOException ex)
+	{
+		System.err.println("Error initializing VideoFileManager or others: " + ex.getMessage());
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public @ResponseBody VideoStatus addVideoData(
+			@PathVariable long id,
+			@RequestParam("data") MultipartFile videoData
+			) throws IOException {
+		
+		if ( videoDataMgr == null ) {
+			videoDataMgr = VideoFileManager.get();
+		}
+		
+		if (!videos.containsKey(id)) {
+			throw new IOException("Cannot find video id: " + Long.toString(id));
+		}
+		
+		if ( !videoData.isEmpty() ) {
+			videoDataMgr.saveVideoData(videos.get(id), videoData.getInputStream());
+		}
+		
+		return new VideoStatus(VideoStatus.VideoState.READY);
 	}
 
 }
