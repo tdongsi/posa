c294936aedad09468e50a21cd1d238326a1779ec
tdongsi <dongsi.tuecuong@gmail.com>
2015-08-25 11:20:59 -0700

    First REST test.

--- RESTfulPhoneBook/src/test/java/com/example/phonebook/PhoneBookTest.java
+++ RESTfulPhoneBook/src/test/java/com/example/phonebook/PhoneBookTest.java
@@ -1,22 +1,39 @@
 package com.example.phonebook;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class PhoneBookTest {
 	
+	private Client client;
+	private WebTarget base;
+	
 	@Test
-	public void f() {
+	public void testGetAll() {
+		WebTarget target = base.path("contacts");
+		Response resp = target.request().get();
+		Object list = resp.getEntity();
+		
+		Assert.assertNotNull(list);
+		Assert.assertEquals(resp.getStatus(), 200);
 	}
 
 	@BeforeClass
 	public void beforeClass() {
-		
+		client = ClientBuilder.newClient();
+		base = client.target("http://localhost:8080/RESTfulPhoneBook/webservice");
 	}
 
 	@AfterClass
 	public void afterClass() {
+		client.close();
 	}
 
 }
